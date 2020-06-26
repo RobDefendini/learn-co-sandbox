0729f7c04c975722c7dd9b65de17d29ff65004d0
Robert Defendini <robdefendini@gmail.com>
2020-06-26 19:07:12 +0000

    Automatically backed up by Learn

---
+++ creating-methods-and-arguments.rb
@@ -0,0 +1,7 @@
+def greeting
+  puts "Hi, Ruby programmer!"
+end
+    #method name      #parameter
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
---
+++ puts_and_prints_statements.rb
@@ -0,0 +1,12 @@
+#Use case for these statements
+puts "Hello World\n"
+Hello World
+ => nil
+ 
+print "Hello World\n"
+Hello World
+ => nil
+ 
+p "Hello World\n"
+"Hello World"
+ => "Hello World\n"
